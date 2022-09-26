3318bc4e16304daa3682a84c0bb2ef1180fd80b2
imadalhannawi <79336508+imadalhannawi@users.noreply.github.com>
2022-09-27 00:19:20 +0200

    ManualPlayer.java

---
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/ManualPlayer.java
@@ -0,0 +1,12 @@
+package fr.icom.info.m1.balleauprisonnier_fx;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class ManualPlayer extends Player{
+
+	ManualPlayer(GraphicsContext gc, String color, int xInit, int yInit, String side) {
+		super(gc, color, xInit, yInit, side);
+
+	}
+	
+}
