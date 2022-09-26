5e19cd1daf807dd81ce616058cb68b9fc02790be
imadalhannawi <79336508+imadalhannawi@users.noreply.github.com>
2022-09-27 00:20:04 +0200

    StaticPlayer.java

---
+++ src/main/java/fr/icom/info/m1/balleauprisonnier_mvn/StaticPlayer.java
@@ -0,0 +1,12 @@
+package fr.icom.info.m1.balleauprisonnier_fx;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class StaticPlayer extends Player {
+
+	StaticPlayer(GraphicsContext gc, String color, int xInit, int yInit, String side) {
+		super(gc, color, xInit, yInit, side);
+
+	}
+
+}
